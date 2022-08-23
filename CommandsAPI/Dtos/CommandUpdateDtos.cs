7be8570db1a09c99dc4a575f25f0899458b659d9
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-08-23 12:57:35 +0530

    update DTO added

---
+++ CommandsAPI/Dtos/CommandUpdateDtos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsAPI.Dtos
+{
+    public class CommandUpdateDtos
+    {
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Plateform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
